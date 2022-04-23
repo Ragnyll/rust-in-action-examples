8af9a9d36c8ca2743ca4bae5111626ed2a4de909
ragnyll <ragnyll@gallowzhumour.dev>
2022-04-23 15:24:53 -0500

    cpu8 adder 2'

--- examples/cpu.rs
+++ examples/cpu.rs
@@ -2,22 +2,25 @@ use rust_in_action::cpu::CPU;
 
 fn main() {
     let mut cpu = CPU {
-        current_operation: 0,
-        registers: [0; 2]
+        registers: [0; 16],
+        memory: [0; 4096],
+        position_in_memory: 0,
     };
 
-    // the opcode the cpu will interpert
-    // - 8 signifies that the operation takes 2 registers
-    // 0 maps to cpu.registers[0]
-    // 1 maps to cpu.registers[1]
-    // 4 indicates addition
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    // initialize registers with some values
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80; mem[1] = 0x14; // loads opcode 0x8014 which adds register 1 to register 0
+    mem[2] = 0x80; mem[3] = 0x24; // loads opcode 0x8024 which adds register 2 to register 0
+    mem[4] = 0x80; mem[5] = 0x34; // loads opcode 0x8034 which adds register 3 to register 0
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 15);
+    assert_eq!(cpu.registers[0], 35);
 
-    println!("5 + 10 = {}", cpu.registers[0]);
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,33 +1,56 @@
 pub struct CPU {
-    pub current_operation: u16,
-    /// the two registers used to add numbers together
-    pub registers: [u8; 2],
+    /// 16 8 bit registers
+    /// can be registered by a single hex number 0 to F
+    pub registers: [u8; 16],
+    /// usually called program_counter
+    pub position_in_memory: usize,
+    pub memory: [u8; 0x1000],
 }
 
 impl CPU {
     pub fn run(&mut self) {
-        // loop {
-        let opcode = self.read_opcode();
+        // process all instructions
+        loop {
+            let opcode = self.read_opcode();
+            // jump to next inestruction
+            self.position_in_memory += 2;
 
-        // opcode decoding
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 0) as u8;
-        // }
+            // opcode decoding
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        match (c, x, y, d) {
-            // dispatches execution to hardware components
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {opcode:04x}")
+                // dispatches execution to hardware components
+            match (c, x, y, d) {
+                // short circuit to return when the opcode 0x0000 is encountered
+                (0, 0, 0, 0) => return,
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {opcode:04x}"),
+            }
         }
     }
 
     fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg_x = self.registers[x as usize];
+        let arg_y = self.registers[y as usize];
+
+        let (val, overflow) = arg_x.overflowing_add(arg_y);
+        self.registers[x as usize] = val;
+
+        // the last bit on the chip 8 reprents overflow
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
